c6af7d3a73ed930da3788a4f2b86cb76d3ce43bb
Asaph <asaphho@yahoo.com.sg>
2021-04-29 22:27:04 +0800

    Added PLAYING flag.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 from datetime import datetime
 import os
 
+PLAYING = False
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -9,8 +11,10 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(10, GPIO.RISING, bouncetime=5000)
 
 while True:
-    if GPIO.event_detected(10):
+    if GPIO.event_detected(10) and not PLAYING:
+        PLAYING = True
         date_str = datetime.strftime(datetime.now(), '%Y-%m-%d')
         os.system(f'mpg321 ~/Downloads/{date_str}.mp3')
+        PLAYING = False
 
 GPIO.cleanup()
