95540655bf837f9eb20e0182e1b354f8ef620739
Ho Canyang Asaph <asaphho@yahoo.com.sg>
2021-04-30 08:57:00 +0800

    Used remove_event_detect rather than cleanup to stop event detection when sleeping.

--- main.py
+++ main.py
@@ -6,19 +6,16 @@ from time import sleep
 INPUT_PIN = 10
 SLEEP_TIME = 30
 
-def start_detecting(input_pin):
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(input_pin, GPIO.RISING, bouncetime=5000)
-
-start_detecting(INPUT_PIN)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=5000)
 
 while True:
     if GPIO.event_detected(INPUT_PIN):
         date_str = datetime.strftime(datetime.now(), '%Y-%m-%d')
         os.system(f'mpg321 /home/pi/Downloads/{date_str}.mp3')
-        GPIO.cleanup()
+        GPIO.remove_event_detect(INPUT_PIN)
         sleep(SLEEP_TIME)
-        start_detecting(INPUT_PIN)
+        GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=5000)
 
