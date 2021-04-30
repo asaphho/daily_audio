78f860db1db5a0514e3b8118ed1ed7eefe62cdc4
Ho Canyang Asaph <asaphho@yahoo.com.sg>
2021-04-30 08:42:49 +0800

    clean up GPIO upon playing and then sleep and then starts detection again.

--- main.py
+++ main.py
@@ -3,22 +3,20 @@ from datetime import datetime
 import os
 from time import sleep
 
-PLAYING = False
-SLEEP_TIME = 30
 INPUT_PIN = 10
+SLEEP_TIME = 30
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=5000)
+def start_detecting(input_pin):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(input_pin, GPIO.RISING, bouncetime=5000)
 
 while True:
-    if GPIO.event_detected(INPUT_PIN) and not PLAYING:
-        PLAYING = True
+    if GPIO.event_detected(INPUT_PIN):
         date_str = datetime.strftime(datetime.now(), '%Y-%m-%d')
         os.system(f'mpg321 /home/pi/Downloads/{date_str}.mp3')
+        GPIO.cleanup()
         sleep(SLEEP_TIME)
-        PLAYING = False
+        start_detecting(INPUT_PIN)
 
-GPIO.cleanup()
