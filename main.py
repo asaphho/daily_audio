98b83a3dcf6f76c7ddccbf7cdfcd2801e555ff44
Ho Canyang Asaph <asaphho@yahoo.com.sg>
2021-04-30 08:27:27 +0800

    Made sleep time and input pin into configurables at top of file.

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import os
 from time import sleep
 
 PLAYING = False
+SLEEP_TIME = 30
+INPUT_PIN = 10
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(10, GPIO.RISING, bouncetime=5000)
+GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=5000)
 
 while True:
-    if GPIO.event_detected(10) and not PLAYING:
+    if GPIO.event_detected(INPUT_PIN) and not PLAYING:
         PLAYING = True
         date_str = datetime.strftime(datetime.now(), '%Y-%m-%d')
         os.system(f'mpg321 /home/pi/Downloads/{date_str}.mp3')
-        sleep(30)
+        sleep(SLEEP_TIME)
         PLAYING = False
 
 GPIO.cleanup()
