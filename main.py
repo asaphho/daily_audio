3b0cbbb862ebcebed7fb629f1cec1414a085b98f
Asaph Ho <asaphho@Uxcs-MacBook-Pro.local>
2021-05-13 22:00:08 +0800

    Refactored main.py to set flag for playing status and sleep for length of file before setting playing flag to False.

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import RPi.GPIO as GPIO
 from datetime import datetime
 import os
 from time import sleep
+from mutagen.mp3 import MP3
 
 INPUT_PIN = 37
-SLEEP_TIME = 30
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=5000)
 
-while True:
-    if GPIO.event_detected(INPUT_PIN):
-        date_str = datetime.strftime(datetime.now(), '%Y-%m-%d')
-        os.system(f'mpg321 /home/pi/Downloads/{date_str}.mp3')
-        GPIO.remove_event_detect(INPUT_PIN)
-        sleep(SLEEP_TIME)
-        GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=5000)
+PLAYING = False
 
+while True:
+    if GPIO.event_detected(INPUT_PIN) and not PLAYING:
+        PLAYING = True
+        date_str = datetime.strftime(datetime.now(), '%Y-%m-%d')
+        path_to_file = f'/home/pi/Downloads/{date_str}.mp3'
+        os.system(f'mpg321 {path_to_file}')
+        audio_file = MP3(path_to_file)
+        length_in_seconds = int(audio_file.info.length)
+        sleep(length_in_seconds)
+        PLAYING = False
