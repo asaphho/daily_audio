0324b5a1f4fd3fe031bba25db1f063abcc3d695f
Asaph Ho <asaphho@Uxcs-MacBook-Pro.local>
2021-05-14 11:28:26 +0800

    Reverted to no attempt to sleep while file is playing. Changed bouncetime to 10 seconds.

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import RPi.GPIO as GPIO
 from datetime import datetime
 import os
-from mutagen.mp3 import MP3
 
 INPUT_PIN = 37
-
-date_str = datetime.strftime(datetime.now(), '%Y-%m-%d')
-path_to_file = f'/home/pi/Downloads/{date_str}.mp3'
-audio_file = MP3(path_to_file)
-length_in_seconds = int(audio_file.info.length)
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=1000*length_in_seconds)
+GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=10000)
 os.system('mpg321 /home/pi/Downloads/ready.mp3')
 
 while True:
-    if GPIO.event_detected(INPUT_PIN):
+    if GPIO.event_detected(INPUT_PIN): 
+        date_str = datetime.strftime(datetime.now(), '%Y-%m-%d')
+        path_to_file = f'/home/pi/Downloads/{date_str}.mp3'
         os.system(f'mpg321 {path_to_file}')
