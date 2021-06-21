e50082c6bd5f17d2de09b39ed9707fc66a996757
Asaph Ho <asaph.ho@univac.com.sg>
2021-06-21 10:45:43 +0800

    Removed the playing of the ready chime

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, bouncetime=10000)
-os.system('mpg321 /home/pi/daily_audio/fixtures/ready.mp3')
 
 while True:
     if GPIO.event_detected(INPUT_PIN): 
